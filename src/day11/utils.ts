218916461c129d52e81b118d0af67c76912e2d82
Kenny R. Lienhard <lienhard@medignition.com>
2024-02-05 16:17:59 +0100

    upgraded packages

--- src/day11/utils.ts
+++ src/day11/utils.ts
@@ -4,3 +4,8 @@ export async function loadData(trainingData = false): Promise<string[][]> {
   const data = await getData(11, trainingData);
   return data.map((line) => line.split(''));
 }
+
+export function printGrid(grid: string[][]) {
+  for (const row of grid) console.log(row.join(''));
+  console.log();
+}
